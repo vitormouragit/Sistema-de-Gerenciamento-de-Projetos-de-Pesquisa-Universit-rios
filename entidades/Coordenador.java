05b8a00d290f74a2d51a77a10d88801666fc5816
Joao Vitor <joaovitorbezerra372@gmail.com>
2026-05-25 20:01:23 -0300

    Criação e divisão dos pacotes

--- entidades/Coordenador.java
+++ entidades/Coordenador.java
@@ -1,3 +1,5 @@
+package entidades;
+
 public class Coordenador extends Usuario{
     public Coordenador(String nome, String email, String senha){
         super(nome, email, senha);
--- entidades/Professor.java
+++ entidades/Professor.java
@@ -1,3 +1,5 @@
+package entidades;
+
 import java.util.ArrayList;
 import java.util.List;
 
