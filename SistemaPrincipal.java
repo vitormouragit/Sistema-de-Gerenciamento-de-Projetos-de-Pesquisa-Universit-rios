38b2c14e53986aaf1db9d99fc3385375232a4f67
Joao Vitor <joaovitorbezerra372@gmail.com>
2026-05-27 13:18:34 -0300

    Sistema Principal

---
+++ SistemaPrincipal.java
@@ -0,0 +1,14 @@
+import entidades.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SistemaPrincipal {
+    private static List<Usuario> listaUsuarios = new ArrayList<>();
+    private static List<Projeto> listaProjetos = new ArrayList<>();
+    
+    private static Usuario usuarioLogado = null; 
+
+    public static void main(String[] args) {
+        
+    }
+}
